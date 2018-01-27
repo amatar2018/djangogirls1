f48f5006e14e98c393ef5971c88e264bca33b170
Sriram Sriniavsan <sriram882004@gmail.com>
2018-01-27 04:46:01 -0600

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,6 +93,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd8vnmh4e0j8a8k',
         'USER': 'copirdqbjtokkn',
