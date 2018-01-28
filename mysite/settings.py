a3e2e47f7c038925c8c759706c448208cf3f176f
Sriram Sriniavsan <sriram882004@gmail.com>
2018-01-28 10:57:08 -0600

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,6 @@ ROOT_URLCONF = 'mysite.urls'
 # Update database configuration with $DATABASE_URL.
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default']=db_from_env
 DATABASES = { 'default': dj_database_url.config() }
 
 
